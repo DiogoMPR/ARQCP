f31229c9cbfb030a035052982df74dbcdf46da01
Diogo <1220812@isep.ipp.pt>
2023-10-23 02:23:40 +0100

    Exercise 8 done, failing 3 tests

--- modulo2/ex08/asm.s
+++ modulo2/ex08/asm.s
@@ -1,50 +1,30 @@
+# This function subtracts two short values, short s1 and short s2, in a crossed fashion.
+# The function should subtracts to the most significant byte s1 the least significant byte of s2 and
+#vice-versa
+
 .section .data
-    .global op1
-    .global op2
-    
-.section .bss
-	.lcomm s1, 2   # Define s1 as a 2-byte variable
-    .lcomm s2, 2   # Define s2 as a 2-byte variable
-
+# declares s1 and s2 as globals and sets their types and values
+	.global s1
+	s1:
+		.short 90
+	
+	.global s2
+	s2:
+		.short 54
+		
 .section .text
-    .global exchangeBytes
-    .global crossSubBytes
 
-exchangeBytes:
-    movw op1(%rip), %ax  # Load op1 into ax
-    movw op2(%rip), %cx  # Load op2 into cx
-    
-    # Extract the most significant byte of op2
-    movb %ch, %dl       # Copy the value in ch to dl
-    
-    # Extract the least significant byte of op1
-    movb %al, %dh       # Copy the value in al to dh
-    
-    # Double the value of the least significant byte of op1
-    addb %dh, %dh       # Double the value in dh
-    
-    # Combine the bytes to form the new short value
-    movb %dl, %al       # Copy the value from dl to al (msb from op2)
-    movb %dh, %ah       # Copy the value from dh to ah (lsb doubled from op1)
-    
-    ret
-   
+#declares crossSubBytes as global
+.global crossSubBytes
+
 crossSubBytes:
- # Subtracts two short values, short s1 and short s2, in a crossed fashion.
- # The function should subtracts to the most significant byte s1 the least significant byte of s2 and vice-versa
-	movw s1(%rip), %ax # load s1 into ax
-	movw s2(%rip), %bx # load s2 into cx
+
+	mov s1(%rip), %ax # load s1 into ax
+	mov s2(%rip), %cx # load s2 into cx
+
+	subb %cl, %ah # Subtract the least significant byte of s2 from the most significant byte of s1
+	subb %al, %ch # Subtract the least significant byte of s1 from the most significant byte of s2
 	
- # Extract the most significant byte of s1 and the least significant byte of s2
-	movb %ah, %ch # copies the value in ah to ch (most significant byte of s1)
-	movb %al, %cl # copies the value in al to cl (least significant byte of s1)
-	movb %bh, %dh # copies the value in bh to dh (most significant byte of s2)
-	movb %bl, %dl # copies the value in bl to dl (least significant byte of s2)
-	
-	subb %dl, %ch # subtracts lsb from s2 to msb from s1
-	subb %cl, %dh # subtracts lsb from s1 to msb from s2
-	
-	movb %ch, %ah # copy the result from ch to ah
-	movb %dh, %al # copy the result from dh to al
+	movb %ch, %al # copies the value in ch to al
 	
 	ret
--- modulo2/ex08/main.c
+++ modulo2/ex08/main.c
@@ -1,18 +1,8 @@
 #include <stdio.h>
 #include "asm.h"
 
-short op1, op2;
-extern short s1, s2;
-
 int main(void) {
-    op1 = 0x1234; // Assign the value 0x1234 to op1
-    op2 = 0x5678; // Assign the value 0x5678 to op2
-
-    short exchangeResult = exchangeBytes();
-    short crossSubResult = crossSubBytes();
-
-    printf("ExchangeBytes Result: 0x%04X\n", exchangeResult);
-    printf("CrossSubBytes Result: 0x%04X\n", crossSubResult);
-
+ 
+    printf("sum: %d\n", crossSubBytes());
     return 0;
 }
