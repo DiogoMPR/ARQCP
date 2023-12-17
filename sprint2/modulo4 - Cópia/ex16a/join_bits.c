0e07f8688476f064fe6119c6e606f3d9feba86a7
Diogo_1220812 <1220812@isep.ipp.pt>
2023-12-17 19:31:16 +0000

    fix

--- sprint2/modulo4 - Cópia/ex16a/join_bits.c
+++
@@ -1,8 +0,0 @@
-long join_bits(long a, long b, char pos){
-	
-	unsigned long generalMask = 0xFFFFFFFFFFFFFFFF;	// mask containing 8 bytes of bits with value 1 : 1111 1111 1111 1111 1111 1111 1111 1111
-	long aMask = generalMask >> (63-pos); //deactivate the irrelevant bits for the mask to apply to a.
-	long bMask = ~aMask; //deactivate the irrelevant bits for the mask to apply to b.
-	long concatenatedValue = (a & aMask) | (b & bMask); //apply the operations that concatenate a and b.
-	return concatenatedValue;
-}
